0910ddf446b638203ab52e73ea46773cc8d0a85f
Richard Lyon <richthegeek@gmail.com>
2013-07-02 15:37:20 +0100

    Create tuning.js

---
+++ tuning.js
@@ -0,0 +1,45 @@
+/*
+ Exploring the effect of initial parameters on collisions and table size.
+ Results have the following columns:
+  accuracy
+  probIncorrect
+  table size
+  number of collisions
+  large collision delta (ie, size of error)
+*/
+
+var sketch = require('count-min-sketch');
+
+var sample = [];
+for (var i = 0; i < 10000; i++) {
+  sample.push(i);
+}
+
+for (var a = 1; a <= 5; a++) {
+	for (var b = 1; b <= 5; b++) {
+		var _a = Math.pow(10, 0 - a);
+		var _b = Math.pow(10, 0 - b);
+		var sk = sketch(_a, _b);
+
+		for (var i = 0; i < 10000; i++) {
+			sk.update(i, 10);
+		}
+
+		var collisions = 0, max = 0;
+		for (var i = 0; i < 10000; i++) {
+			var value = sk.query(i);
+			if (value != 10) {
+				collisions++;
+				max = Math.max(max, value);
+			}
+		}
+		var size = Math.round(sk.table.length / 1024);
+
+		var pad = function(val) {
+			val = val.toString();
+			val = val + ('          ').substring(0, 10 - val.length);
+			return val;
+		}
+		console.log.apply(console.log, [_a, _b, size + 'kb', collisions, max].map(pad));
+	}
+}
